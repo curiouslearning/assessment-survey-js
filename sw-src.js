7a2aec14a677edd11ed1c1c395c750a2a3ea7688
george dzavashvili <dzavashviligeorge@gmail.com>
2023-12-14 00:17:42 +0400

    SW version bump to 1.2

--- sw-src.js
+++ sw-src.js
@@ -7,7 +7,7 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
 
 const channel = new BroadcastChannel("as-message-channel");
 
-let version = 1.1;
+let version = 1.2;
 let cachingProgress = 0;
 let cachableAssetsCount = 0;
 
