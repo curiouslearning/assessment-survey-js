8f27bc7c9eb05ff9bab9be6327e968c12e472eb9
george dzavashvili <dzavashviligeorge@gmail.com>
2024-03-28 21:12:08 +0400

    Comment out the bucket id randomization

--- src/App.ts
+++ src/App.ts
@@ -15,7 +15,7 @@ import { Workbox } from 'workbox-window';
 import CacheModel from './components/cacheModel';
 import { UIController } from './components/uiController';
 
-const appVersion = "v0.2.4";
+const appVersion = "v0.2.5";
 
 let loadingScreen = document.getElementById("loadingScreen");
 
