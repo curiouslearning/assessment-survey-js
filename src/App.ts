b89d5290ac51d3671f559ab1c5e2ef142d8921cb
george dzavashvili <dzavashviligeorge@gmail.com>
2024-03-28 18:02:54 +0400

    Version bump to v0.2.2

--- src/App.ts
+++ src/App.ts
@@ -15,7 +15,7 @@ import { Workbox } from 'workbox-window';
 import CacheModel from './components/cacheModel';
 import { UIController } from './components/uiController';
 
-const appVersion = "v0.2.1";
+const appVersion = "v0.2.2";
 
 let loadingScreen = document.getElementById("loadingScreen");
 
