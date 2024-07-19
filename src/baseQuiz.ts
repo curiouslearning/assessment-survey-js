21135757499bacd3460e07e9bbc049cbbcba0212
george dzavashvili <dzavashviligeorge@gmail.com>
2024-07-19 07:50:01 +0400

    Initial commit for dev mode

--- src/baseQuiz.ts
+++ src/baseQuiz.ts
@@ -7,6 +7,33 @@ export abstract class BaseQuiz {
 	protected app: App;
 	public dataURL: string;
 	public unityBridge: UnityBridge;
+	
+	public devModeAvailable: boolean = false;
+	public isInDevMode: boolean = false;
+
+	public devModeToggleButtonContainerId: string = "devModeModalToggleButtonContainer";
+	public devModeToggleButtonContainer: HTMLElement;
+
+	public devModeToggleButtonId: string = "devModeModalToggleButton";
+	public devModeToggleButton: HTMLButtonElement;
+
+	public devModeModalId: string = "devModeSettingsModal";
+	public devModeSettingsModal: HTMLElement;
+
+	constructor() {
+		this.devModeToggleButtonContainer = document.getElementById(this.devModeToggleButtonContainerId);
+		this.devModeToggleButton = document.getElementById(this.devModeToggleButtonId) as HTMLButtonElement;
+		this.devModeSettingsModal = document.getElementById(this.devModeModalId);
+		this.devModeToggleButton.onclick = this.ToggleDevModeModal;
+	}
+
+	public ToggleDevModeModal = () => {
+		if (this.devModeSettingsModal.style.display == "block") {
+			this.devModeSettingsModal.style.display = "none";
+		} else {
+			this.devModeSettingsModal.style.display = "block";
+		}
+	}
 
 	public abstract Run(applink: App): void;
 	public abstract TryAnswer(ans: number, elapsed: number): void;
