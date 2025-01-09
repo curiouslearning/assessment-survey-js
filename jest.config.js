c53fc0ef8514030e24c66477a9be29ccc7557637
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2025-01-09 18:21:59 +0530

    Updating configurations for unit testing!

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'], // Include files for coverage
+  collectCoverageFrom: ['src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
-      branches: 80, // Minimum branch coverage %
-      functions: 80, // Minimum function coverage %
-      lines: 80, // Minimum line coverage %
-      statements: 80, // Minimum statement coverage %
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
