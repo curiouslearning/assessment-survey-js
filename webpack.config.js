1103e5410cdcf75854825276485981c69a036030
Miguel Felipe G Calo <miguelc0611@yahoo.com>
2026-03-24 12:51:10 +0800

    fix: quick fix for build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
 const isDev = (env !== 'production');
@@ -8,6 +9,16 @@ module.exports = {
   entry: './src/App.ts',
   mode,
   devtool: isDev ? 'inline-source-map' : false,
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'public'), 
+          to: path.resolve(__dirname, 'dist') 
+        },
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
