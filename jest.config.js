e3a2097737e9f4380e9c1a5caae86566a04ca3a1
ashwinnair-chimple <ashwin.nair@sutara.org>
2026-03-24 13:36:47 +0530

    chore:comments resolved

--- jest.config.js
+++ jest.config.js
@@ -16,10 +16,10 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
