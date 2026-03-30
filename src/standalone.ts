f17f58585c1bf3dbdd6c1c0a3ff8d3f7bbb0895a
Bernhard Cena <bernhardc@codev.com>
2026-03-30 11:57:11 +0800

    chore: Re-added analytics-config but only available for standalone version (FM-868)

--- src/standalone.ts
+++ src/standalone.ts
@@ -1,6 +1,7 @@
 import { startStandaloneApp } from './App';
 import { AnalyticsIntegration } from '@analytics/analytics-integration';
 import { mountAssessmentSurveyFragment, normalizeBaseUrl, normalizeHostTheme } from '@ui/dom-template';
+import { firebaseConfig } from '@analytics/analytics-config';
 
 const STANDALONE_ROOT_ID = 'assessment-survey-root';
 const DEFAULT_ASSET_BASE_URL = '/assets';
@@ -63,14 +64,14 @@ mountStandaloneTemplate(standaloneRoot, assetBaseUrl);
 
 async function main() {
     await AnalyticsIntegration.initializeAnalytics({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        apiKey: firebaseConfig.apiKey,
+        authDomain: firebaseConfig.authDomain,
+        databaseURL: firebaseConfig.databaseURL,
+        projectId: firebaseConfig.projectId,
+        storageBucket: firebaseConfig.storageBucket,
+        messagingSenderId: firebaseConfig.messagingSenderId,
+        appId: firebaseConfig.appId,
+        measurementId: firebaseConfig.measurementId,
     });
 
     startStandaloneApp({ assetBaseUrl });
