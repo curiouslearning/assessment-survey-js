e05c33d778ee6c7f1d47797c652e696babcd6af9
Bernhard Cena <bernhardc@codev.com>
2026-06-02 15:33:54 +0800

    fix: Standalone Assessment app shows Blue screen prior to loader (FM-918).

--- src/ui/dom-template/sections/shared/loading-screen-section.ts
+++ src/ui/dom-template/sections/shared/loading-screen-section.ts
@@ -2,7 +2,11 @@ import { appendChildren, createElement, TemplateSection } from '../../assessment
 
 export class LoadingScreenSection extends TemplateSection<HTMLDivElement> {
   public render(): HTMLDivElement {
-    const loadingScreen = createElement('div', { id: 'loadingScreen' });
+    const loadingScreen = createElement('div', {
+      id: 'loadingScreen',
+      style:
+        'position:absolute;top:0;left:0;width:100%;height:100%;display:flex;justify-content:center;align-items:center;z-index:100;background-color:white;flex-direction:column',
+    });
     const loadingGif = createElement('img', {
       id: 'loading-gif',
       attrs: {
